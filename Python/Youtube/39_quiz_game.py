a08cf8f3e7df3c749e952053f26dd084d090997e
diepAu13122001 <91660142+diepAu13122001@users.noreply.github.com>
2023-08-16 16:03:42 +0700

    day 4

---
+++ Python/Youtube/39_quiz_game.py
@@ -0,0 +1,19 @@
+# -------------------------------------------------
+def new_game():
+    pass
+# -------------------------------------------------
+def check_answer():
+    pass
+# -------------------------------------------------
+def display_score():
+    pass
+# -------------------------------------------------
+def play_again():
+    pass
+
+questions = {
+    "Who created Python?": "A",
+    "What year was Python created?": "B",
+    "Python is a tribute to which comedy group?": "C",
+    "Is the Earth round?": "A"
+} 
