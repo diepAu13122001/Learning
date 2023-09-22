75e4c48d37a0ca3a2c20648e9ba5a16cfea79c9d
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2023-09-23 03:26:50 +0700

    day 5

---
+++ Python/Youtube/52_functions_to_variables.py
@@ -0,0 +1,13 @@
+# gan bien bang voi ten cua 1 function -> dat them ten cho function do
+
+def hello(text):
+    print(text)
+
+print(hello) # dia chi luu function trong bo nho
+hi = hello # gan bien bang function hello ( k co () nha)
+print(hi)
+hello("Hi friends")
+hi("Hi girls")
+
+say = print
+say("Whoa! I can't believe this works! :O")
