7858b9526a1b2822f2016f70dd3d6600b0d13b2b
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2023-11-14 11:44:16 +0700

    Lab7

---
+++ Java/AI/Code/Lab7/src/code/Test.java
@@ -0,0 +1,16 @@
+package code;
+
+public class Test {
+	public static void main(String[] args) {
+		Node node = new Node();
+
+		System.out.println("--------------------------");
+		GA_NQueenAlgo alg = new GA_NQueenAlgo();
+		long startTime = System.currentTimeMillis();
+		alg.execute().displayBoard();
+		long endTime = System.currentTimeMillis();
+
+		System.out.println("Max iteration: " + GA_NQueenAlgo.MAX_ITERATIONS);
+		System.out.println("Running time: " + (endTime - startTime) / 1000 + "s");
+	}
+}
