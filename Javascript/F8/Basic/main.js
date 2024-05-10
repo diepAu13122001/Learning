ac45390035410d5709eaae7cbe1c2382b6e47b02
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2024-05-10 12:07:15 +0700

    update

---
+++ Javascript/F8/Basic/main.js
@@ -0,0 +1,9 @@
+// input, output ---------------------------------------------
+alert("Day la thong bao")
+prompt("Day la phan can nhap vao: ")
+
+// Variable ---------------------------------------------------
+var fullName = "Au Ngoc Diep";
+// dau "=": gan, assign
+
+// Comment ---------------------------------------------------
