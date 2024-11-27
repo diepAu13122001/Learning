2ae68b90f0afd1cbefc7fc6e3f6f6deae3b33deb
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2024-11-27 17:37:03 +0700

    js advanced 2

---
+++ Javascript/F8/Advance/4.4_primitive_reference_types.js
@@ -0,0 +1,15 @@
+/**
+ * [Value types: Primitive types]: tai 1 thoi diem chi chua 1 gia tri cu the
+ * - String
+ * - Boolean
+ * - Number
+ * - BigInt
+ * - Symbol
+ * - undefined
+ * - null
+ * -------------------------------------------
+ * [Reference types: Non-primitive types]
+ * - Object
+ * - Array
+ * - Function
+ */
