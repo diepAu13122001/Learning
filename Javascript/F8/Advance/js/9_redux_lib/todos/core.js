ab704f745744f674d982f39fbc8fcd838bb48dad
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2025-01-18 13:09:29 +0700

    redux

---
+++ Javascript/F8/Advance/js/9_redux_lib/todos/core.js
@@ -0,0 +1,51 @@
+// Tạo thư viện tự viết thẻ HTML
+export default function html([first, ...strings], ...values) {
+  // cong don code html vao list => hien thi duoi dang string
+  return values
+    .reduce(
+      (acc, cur) => {
+        acc.concat(cur, strings.shift());
+      },
+      [first]
+    )
+    .filter((x) => (x && x !== true) || x === 0) // neu khong co gia tri thi xoa
+    .join("");
+}
+
+export function createStore(reducer) {
+  // tạo reducer với trạng thái hiện tại là undefined, phần action là {}
+  let state = reducer(undefined, {});
+  const roots = new Map();
+  // phương thức cần thiết
+
+  function render() {
+    // load cac the len giao dien
+    for (const [root, component] of root) {
+      const output = component();
+      root.innerHTML = output;
+    }
+  }
+  return {
+    // ham them du lieu tao lenh HTML moi
+    attach(component, root) {
+      roots.set(root, component);
+      render();
+    },
+    // xoa du lieu
+    dispatch(action, ...args) {
+      state = reducer(state, action, args);
+      render();
+    },
+    // ham day du lieu tu store vao view (selector la lua chon du lieu can hien thi)
+    // selector mặc định là 1 hàm; (state) => state để duyệt qua tất cả phần tử trong state
+    connect(selector = (state) => state) {
+      // return lại 1 arrow function(return thêm 1 arrow function nữa)
+      return (component) =>
+        (props, ...args) => {
+          // truyen het du lieu vao component dưới dạng object
+          // vì các thuộc tính đều có khả năng là object nên cần thêm Object.assign để truyền thành thuộc tính
+          component(Object.assign({}, props, selector(state), ...args));
+        };
+    },
+  };
+}
