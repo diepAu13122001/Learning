6458090c46e85b2d5cb562aa0e9c625fc552d6d4
diepan-mindx <diepau1312@mindx.net.vn>
2025-02-16 13:40:38 +0700

    react webpack

---
+++ Javascript/ReactJS/10.ReactWebpacks/src/index.js
@@ -0,0 +1,19 @@
+import React from "react"; // nạp thư viện react
+import ReactDOM from "react-dom"; // nạp thư viện react-dom
+import { createRoot } from 'react-dom/client';
+
+// Tạo component App
+function App() {
+  return (
+    <div>
+      <h1>Xin chào anh em F8!</h1>
+    </div>
+  );
+}
+
+// get root element
+const rootElement = document.getElementById("root");
+// React-DOM => render UI
+const rootReact = createRoot(rootElement);
+// add element
+rootReact.render(<App />);
